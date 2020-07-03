28c01539a11e173a5349fc769e89e552cc847168
pooja <pooja@gmail.com>
2020-07-03 20:41:07 +0530

    added color

--- app.js
+++ app.js
@@ -8,20 +8,24 @@
         $scope.validityMsg ="";
         $scope.lunch=null;
         $scope.count=0;
+        $scope.color="black"
         $scope.checkValue = function () {
             $scope.validityMsg ="Value Entered";
         };
         $scope.findCount=function(){
             if(!$scope.lunch){
-                $scope.validityMsg="Please enter data first"
+                $scope.validityMsg="Please enter data first!"
+                $scope.color="red"
             }
             const arr = $scope.lunch.split(",");
             var val=arr.length;
             $scope.count=val;
             if($scope.count<=3){
                 $scope.validityMsg="Enjoy!"
+                $scope.color="green"
             }if($scope.count>3){
                 $scope.validityMsg="Too Much!"
+                $scope.color="green"
             }
             
         };
